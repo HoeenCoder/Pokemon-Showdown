8eeed029afcc5b7900ea84c491f4d475a7d8c21a
HoeenHero <HoeenCoder@users.noreply.github.com>
2017-05-10 07:57:21 -0400

    |notify|title|message

--- chat-commands.js
+++ chat-commands.js
@@ -1990,7 +1990,7 @@ exports.commands = {
 		if (!this.can('declare', null, room)) return false;
 		if (!this.canTalk()) return;
 
-		this.add(`|notify|${Chat.escapeHTML(target)}`);
+		this.add(`|notify|The staff in ${room.id} made a declaration|${Chat.escapeHTML(target)}`);
 		this.add(`|raw|<div class="broadcast-blue"><b>${Chat.escapeHTML(target)}</b></div>`);
 		this.logModCommand(user.name + " declared " + target);
 	},
@@ -2003,7 +2003,7 @@ exports.commands = {
 		target = this.canHTML(target);
 		if (!target) return;
 
-		this.add(`|notify|${target}`);
+		this.add(`|notify|The staff in ${room.id} made a declaration|${target}`);
 		this.add(`|raw|<div class="broadcast-blue"><b>'${target}</b></div>`);
 		this.logModCommand(user.name + " declared " + target);
 	},
