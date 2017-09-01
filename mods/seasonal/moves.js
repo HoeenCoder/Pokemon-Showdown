b663914086e690c26daa4ca48209e9dbdb574358
AndrewGoncel <andregalveia@gmail.com>
2017-09-02 00:50:18 +0100

    Add Trickster's custom move (WIP)

--- mods/seasonal/moves.js
+++ mods/seasonal/moves.js
@@ -127,4 +127,48 @@ exports.BattleMovedex = {
 		target: "allAdjacent",
 		type: "Electric",
 	},
+	//Trickster
+	'3freeze': {
+		accuracy: 100,
+		basePower: 0,
+		basePowerCallback: function (pokemon, target) {
+			let targetWeight = target.getWeight();
+			if (targetWeight >= 200) {
+				return 120;
+			}
+			if (targetWeight >= 100) {
+				return 100;
+			}
+			if (targetWeight >= 50) {
+				return 80;
+			}
+			if (targetWeight >= 25) {
+				return 60;
+			}
+			if (targetWeight >= 10) {
+				return 40;
+			}
+			return 20;
+		},
+		category: 'Special',
+		id: '3freeze',
+		name: '3 Freeze',
+		isNonstandard: true,
+		pp: 10,
+		priority: 0,
+		flags: {contact: 1, protect: 1, mirror: 1},
+		onStart: function (source) {
+			this.setPseudoWeather('gravity');
+		},
+		onTryHit: function (target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', source, "Psyshock", target); //Still needs confirmation
+		},
+		secondary: {
+			chance: 10,
+			volatileStatus: 'flinch',
+		},
+		target: "normal",
+		type: "Psychic",
+	},
 };
