d831d7e80f3a81489bc903a7432067b80c03a57f
Totem Lurantis <worstdigiroommod@gmail.com>
2018-08-24 23:48:45 -0400

    Update random-teams.js

--- mods/seasonal/random-teams.js
+++ mods/seasonal/random-teams.js
@@ -232,10 +232,10 @@ class RandomStaffBrosTeams extends RandomTeams {
 		while (pool.length && team.length < 6) {
 			let name = '';
 			// DEBUG CODE
-			let debug = true; // Programmers - Toggle this to use the code below
+			let debug = false; // Programmers - Toggle this to use the code below
 			if (team.length === 1 && debug) {
 				// Force a specific set to appear for testing
-				name = 'DragonWhale';
+				name = 'Kalalokki';
 				if (pool.indexOf(name) > -1) pool.splice(pool.indexOf(name), 1);
 			} else {
 				name = this.sampleNoReplace(pool);
