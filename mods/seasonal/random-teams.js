64d8d6d0dd524129acb5c0293e3edab780ef1228
Totem Lurantis <worstdigiroommod@gmail.com>
2018-08-31 19:43:57 -0400

    Update randbats set nui
    [19:37:16] nui: i guess
    [19:37:18] nui: ultimately
    [19:37:24] nui: i shoudln't have the option for rest and
    [19:37:26] nui: sleeptalk
    [19:37:28] nui: i should just keep it
    [19:37:31] nui: recover toxic
    [19:37:32] nui: custom
    [19:37:36] nui: and steam eruption
    @moo left
    [19:37:43] %Lycanium Z: ok
    [19:39:48] %Lycanium Z: Do you want me to commit that as well?
    [19:41:59] nui: yea
    [19:42:01] nui: please
    [19:42:04] nui: cause might as well

--- mods/seasonal/random-teams.js
+++ mods/seasonal/random-teams.js
@@ -217,7 +217,7 @@ class RandomStaffBrosTeams extends RandomTeams {
 			},
 			'nui': {
 				species: 'Milotic', ability: 'Prismatic Terrain', item: 'Waterium Z', gender: 'N',
-				moves: ['Steam Eruption', ['Toxic', 'Sleep Talk'], ['Recover', 'Rest']],
+				moves: ['Steam Eruption', 'Toxic', 'Recover',],
 				signatureMove: 'Pyramiding Song',
 				evs: {hp: 252, def: 252, spd: 4}, ivs: {atk: 0}, nature: 'Bold', shiny: true,
 			},
@@ -323,15 +323,6 @@ class RandomStaffBrosTeams extends RandomTeams {
 				let move = this.sampleNoReplace(ssbSet.moves);
 				if (Array.isArray(move)) move = this.sampleNoReplace(move);
 				set.moves.push(move);
-			}
-			if (name === 'nui') { //  nui must always have these sleep talk and rest if it has 1 or the other.
-				if (set.moves.includes('rest') && !set.moves.includes('sleeptalk')) {
-					set.moves.splice(set.moves.indexOf('rest') + 1, 1);
-					set.moves.push('sleeptalk');
-				} else if (!set.moves.includes('rest') && set.moves.includes('sleeptalk')) { // separated for positioning
-					set.moves.splice(set.moves.indexOf('sleeptalk') - 1, 1);
-					set.moves.push('rest');
-				}
 			}
 			set.moves.push(ssbSet.signatureMove);
 			team.push(set);
