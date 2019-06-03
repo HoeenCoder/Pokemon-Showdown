c8618ba48fb6e1cb80df98b9402ffd412a475a25
fart <tyler.m.sciara@gmail.com>
2019-06-03 18:27:11 -0400

    Update Sunny's Mega ability

--- data/mods/ssb/pokedex.js
+++ data/mods/ssb/pokedex.js
@@ -133,6 +133,11 @@ let BattlePokedex = {
 		inherit: true,
 		abilities: {0: 'Levitate'},
 	},
+	// Sunny
+	sceptilemega: {
+		inherit: true,
+		abilities: {0: 'Contrary'},
+	},
 	// Teclis
 	gallademega: {
 		inherit: true,
