c6cda03c4705144d92f23d06c6bb455763ca9045
fart <tyler.m.sciara@gmail.com>
2019-06-10 15:48:03 -0400

    Add Kris mega stats

--- data/mods/ssb/pokedex.js
+++ data/mods/ssb/pokedex.js
@@ -66,6 +66,27 @@ let BattlePokedex = {
 		inherit: true,
 		abilities: {0: 'Poison Heal'}, 	// Ability change is permanent
 	},
+	// Kris
+	rotomfan: {
+		inherit: true,
+		baseStats: {hp: 50, atk: 65, def: 127, spa: 145, spd: 127, spe: 106},
+	},
+	rotomfrost: {
+		inherit: true,
+		baseStats: {hp: 50, atk: 65, def: 127, spa: 145, spd: 127, spe: 106},
+	},
+	rotomheat: {
+		inherit: true,
+		baseStats: {hp: 50, atk: 65, def: 127, spa: 145, spd: 127, spe: 106},
+	},
+	rotommow: {
+		inherit: true,
+		baseStats: {hp: 50, atk: 65, def: 127, spa: 145, spd: 127, spe: 106},
+	},
+	rotomwash: {
+		inherit: true,
+		baseStats: {hp: 50, atk: 65, def: 127, spa: 145, spd: 127, spe: 106},
+	},
 	// Level 51
 	porygon2: {
 		inherit: true,
