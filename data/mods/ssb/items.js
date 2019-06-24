7dcd1789befb9cb86a23115095ee2f61d9853d1a
fart <tyler.m.sciara@gmail.com>
2019-06-24 19:29:19 -0400

    Update items.js

--- data/mods/ssb/items.js
+++ data/mods/ssb/items.js
@@ -38,6 +38,18 @@ let BattleItems = {
 		gen: 7,
 		desc: "If held by a Golem with Rock Slide, it can use Rickrollout.",
 	},
+	// Kris
+	thunderstone: {
+		inherit: true,
+		megaStone: "Rotom-Wash",
+		megaEvolves: "Rotom",
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseTemplate.baseSpecies) return false;
+			return true;
+		},
+		fling: undefined, // Cannot be flung now since its a mega stone
+		desc: "If held by a Rotom, this item allows it to Mega Evolve in battle.",
+	},
 	// MajorBowman
 	victiniumz: {
 		id: "victiniumz",
