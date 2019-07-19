a5b585561130cf1ea1c143b328e4771558054d50
fart <tyler.m.sciara@gmail.com>
2019-07-18 21:17:34 -0400

    Update moves.js (#195)

--- data/mods/ssb/moves.js
+++ data/mods/ssb/moves.js
@@ -522,7 +522,10 @@ let BattleMovedex = {
 			this.add('-message', `${oldName} was sent to the distortion world and replaced with somebody else!`);
 			for (let stat of Object.keys(target.boosts)) {
 				// @ts-ignore Iterate through stat changes to update client
-				this.add('-setboost', target, stat, target.boosts[stat], '[silent]');
+				if (target.boosts[stat] !== 0) {
+					// @ts-ignore Iterate through stat changes to update client
+					this.add('-setboost', target, stat, target.boosts[stat], '[silent]');
+				}
 			}
 		},
 		target: "normal",
