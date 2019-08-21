e7d4100c8e3931bb87f69a634f8cf75be4f50e0a
Volco <13051425+Volco@users.noreply.github.com>
2019-08-21 10:51:23 -0400

    SSB: Sunny -> Sundar

--- data/mods/ssb/random-teams.js
+++ data/mods/ssb/random-teams.js
@@ -644,17 +644,17 @@ class RandomStaffBrosTeams extends RandomTeams {
 				signatureMove: 'Kratosmana',
 				evs: {hp: 4, spa: 252, spe: 252}, nature: 'Naive', shiny: true,
 			},
+			'Sundar': {
+				species: 'Sceptile', ability: 'Overgrow', item: 'Sceptilite', gender: 'M',
+				moves: ['Sludge Wave', 'Draco Meteor', 'Focus Blast'],
+				signatureMove: 'Leaf Blaster',
+				evs: {def: 4, spa: 252, spe: 252}, nature: 'Timid', shiny: true,
+			},
 			'SunGodVolcarona': {
 				species: 'Volcarona', ability: 'Solar Flare', item: 'Volcaronium Z', gender: 'M',
 				moves: ['Quiver Dance', 'Giga Drain', ['Earth Power', 'Psychic']],
 				signatureMove: 'Fiery Dance',
 				evs: {spa: 252, spd: 4, spe: 252}, ivs: {atk: 0}, nature: 'Timid',
-			},
-			'Sunny': {
-				species: 'Sceptile', ability: 'Overgrow', item: 'Sceptilite', gender: 'M',
-				moves: ['Sludge Wave', 'Draco Meteor', 'Focus Blast'],
-				signatureMove: 'Leaf Blaster',
-				evs: {def: 4, spa: 252, spe: 252}, nature: 'Timid', shiny: true,
 			},
 			'Teclis': {
 				species: 'Darkrai', ability: 'Dark Aura', item: 'Darkrainium Z', gender: 'N',
