1e3339754d69a0e1fd0fb813603207c73941a301
fart <tyler.m.sciara@gmail.com>
2019-09-02 22:19:02 -0400

    Update moves.js

--- data/mods/ssb/moves.js
+++ data/mods/ssb/moves.js
@@ -940,11 +940,11 @@ let BattleMovedex = {
 		pp: 5,
 		priority: 0,
 		flags: {dance: 1, heal: 1},
-		heal: [1, 3],
 		onTryMove(pokemon) {
 			this.attrLastMove('[still]');
 		},
 		onHit(target, source, effect) {
+			this.heal(source.maxhp / 3, source);
 			let dancemoves = ['dragondance', 'featherdance', 'fierydance', 'petaldance', 'quiverdance', 'revelationdance', 'swordsdance', 'teeterdance'];
 			let randomMove = dancemoves[this.random(dancemoves.length)];
 			this.useMove(randomMove, target);
