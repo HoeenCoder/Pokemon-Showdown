0d343d114a62a5f9c3df956f04b45231ff39f315
fart <24866009+tmagicturtle@users.noreply.github.com>
2020-06-04 23:54:42 -0400

    Update M-Lop's typing

--- data/mods/ssb/pokedex.ts
+++ data/mods/ssb/pokedex.ts
@@ -6,6 +6,11 @@ export const BattlePokedex: {[k: string]: ModdedSpeciesData} = {
 		baseStats: {hp: 100, atk: 100, def: 100, spa: 100, spd: 100, spe: 100}, // the base stats for the pokemon
 	},
 	*/
+	// Kaiju Bunny
+	lopunnymega: {
+		inherit: true,
+		types: ['Normal', 'Fairy'],
+	},
 	// OM~!
 	magneton: {
 		inherit: true,
