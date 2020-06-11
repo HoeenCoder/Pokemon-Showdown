dde809c986c8ea9b00952317489e7f0ddd44d201
fart <24866009+tmagicturtle@users.noreply.github.com>
2020-06-11 16:55:03 -0400

    Update statuses.ts

--- data/mods/ssb/statuses.ts
+++ data/mods/ssb/statuses.ts
@@ -154,7 +154,7 @@ export const BattleStatuses: {[k: string]: ModdedPureEffectData} = {
 			activeMon = toID(pokemon.side.foe.active[0].illusion ? pokemon.side.foe.active[0].illusion.name : activeMon = pokemon.side.foe.active[0].name);
 			let family = ['aethernum', 'flare', 'trickster', 'celestial', 'gimm1ck', 'zalm', 'aelita', 'biggie', 'celine', 'sundar'];
 			if (family.includes(activeMon)) {
-				this.add(`c|${getName('fart')}|At least I... wasn't boring, right?`);
+				this.add(`c|${getName('fart')}|at least I wasn't boring, right?`);
 			} else {
 				this.add(`c|${getName('fart')}|oy, I die`);
 			}
