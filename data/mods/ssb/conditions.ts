38409eb6a31afd2623f286311c07c5ae4b016f24
PerishSong <69888037+PerishSong@users.noreply.github.com>
2020-08-19 09:45:37 +0300

    Update conditions.ts

--- data/mods/ssb/conditions.ts
+++ data/mods/ssb/conditions.ts
@@ -1089,6 +1089,18 @@ export const Conditions: {[k: string]: ModdedConditionData} = {
 			this.add(`c|${getName('Tenshi')}|Grr bork bork :(`);
 		},
 	},
+	thewaffleman: {
+      noCopy: true,
+      onStart() {
+          this.add(`c|${getName('thewaffleman')}|Whats Good Youtube its your boy thewaffleman`);
+      },
+        onSwitchOut() {
+          this.add(`c|${getName('thewaffleman')}|Never Gonna Give You Up`);
+      },
+      onFaint() {
+          this.add(`c|${getName('thewaffleman')}|coyg`);
+      },
+   },
 	tiki: {
 		noCopy: true,
 		onStart() {
