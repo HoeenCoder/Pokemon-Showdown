dda6112547a331232d5580100b3e411d8ad732a6
Instruct <66930476+xInstruct@users.noreply.github.com>
2020-08-26 03:39:06 -0400

    Fix spacing

--- data/mods/ssb/random-teams.ts
+++ data/mods/ssb/random-teams.ts
@@ -616,10 +616,10 @@ export const ssbSets: SSBSets = {
 		evs: {hp: 4, spa: 252, spe: 252}, nature: 'Timid',
 	},
 	Zyg: {
-	species: 'Azelf', ability: 'Magic Bounce', item: ['Life Orb', 'Expert Belt'], gender: 'M',	
-	moves: ['Photon Geyser', 'Knock Off', ['U-turn', 'Play Rough', 'Close Combat']],
-	signatureMove: 'Luck of the Draw',
-	evs: {atk: 252, def: 4, spe:252}, nature: 'Timid',
+		species: 'Azelf', ability: 'Magic Bounce', item: ['Life Orb', 'Expert Belt'], gender: 'M',	
+		moves: ['Photon Geyser', 'Knock Off', ['U-turn', 'Play Rough', 'Close Combat']],
+		signatureMove: 'Luck of the Draw',
+		evs: {atk: 252, def: 4, spe:252}, nature: 'Timid',
 	},
 };
 
