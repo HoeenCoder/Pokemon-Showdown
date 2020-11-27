936c06d43e95953377962b2578cde5be0f3509a9
Instruct <66930476+xInstruct@users.noreply.github.com>
2020-11-26 20:48:28 -0500

    OK actually do this

--- data/mods/ssb/moves.ts
+++ data/mods/ssb/moves.ts
@@ -3810,8 +3810,9 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 			onTerrain(pokemon) {
 				if ((pokemon.hasType('Water') || pokemon.hasType('Ground')) && pokemon.isGrounded() && !pokemon.isSemiInvulnerable()) {
 					this.debug('Pokemon is grounded and a Water or Ground type, healing through Swampy Terrain.');
-					this.heal(pokemon.baseMaxhp / 16, pokemon, pokemon);
-					this.add('-message', `${pokemon.name} was healed by the terrain!`);
+					if (this.heal(pokemon.baseMaxhp / 16, pokemon, pokemon)) {
+						this.add('-message', `${pokemon.name} was healed by the terrain!`);
+					}
 				}
 			},
 			onEnd() {
@@ -4162,15 +4163,17 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 			onHit(target, source, move) {
 				if (!target.hp || target.species.name !== 'Mismagius') return;
 				if (move?.effectType === 'Move' && move.category !== 'Status') {
-					this.boost({spe: 1}, target);
-					this.add('-message', `${target.name} got a boost by the terrain!`);
+					if (this.boost({spe: 1}, target)) {
+						this.add('-message', `${target.name} got a boost by the terrain!`);
+					}
 				}
 			},
 			onSwitchInPriority: -1,
 			onSwitchIn(target) {
 				if (target?.species.name === 'Mismagius') {
-					this.boost({spa: 1, spd: 1}, target);
-					this.add('-message', `${target.name} got a boost by the terrain!`);
+					if (this.boost({spa: 1, spd: 1}, target)) {
+						this.add('-message', `${target.name} got a boost by the terrain!`);
+					}
 				}
 			},
 			onStart(battle, source, effect) {
@@ -4181,8 +4184,9 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 				}
 				this.add('-message', 'The battlefield became dark!');
 				if (source?.species.name === 'Mismagius') {
-					this.boost({spa: 1, spd: 1}, source);
-					this.add('-message', `${source.name} got a boost by the terrain!`);
+					if (this.boost({spa: 1, spd: 1}, source)) {
+						this.add('-message', `${source.name} got a boost by the terrain!`);
+					}
 				}
 			},
 			onResidualOrder: 5,
@@ -4193,8 +4197,9 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 			onTerrain(pokemon) {
 				if (!pokemon.isSemiInvulnerable()) {
 					if (pokemon && !pokemon.hasType('Ghost')) {
-						this.damage(pokemon.baseMaxhp / 16, pokemon);
-						this.add('-message', `${pokemon.name} was hurt by the terrain!`);
+						if (this.damage(pokemon.baseMaxhp / 16, pokemon)) {
+							this.add('-message', `${pokemon.name} was hurt by the terrain!`);
+						}
 					}
 				}
 			},
@@ -4625,11 +4630,13 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 			onTerrain(pokemon) {
 				if (pokemon.hasType('Ground')) return;
 				if (pokemon.hasType('Electric')) {
-					this.heal(pokemon.baseMaxhp / 8, pokemon);
-					this.add('-message', `${pokemon.name} was healed by the terrain!`);
+					if (this.heal(pokemon.baseMaxhp / 8, pokemon)) {
+						this.add('-message', `${pokemon.name} was healed by the terrain!`);
+					}
 				} else if (!pokemon.hasType('Electric') && (pokemon.hasType(['Flying', 'Steel']) || pokemon.hasAbility('levitate'))) {
-					this.damage(pokemon.baseMaxhp / 8, pokemon);
-					this.add('-message', `${pokemon.name} was hurt by the terrain!`);
+					if (this.damage(pokemon.baseMaxhp / 8, pokemon)) {
+						this.add('-message', `${pokemon.name} was hurt by the terrain!`);
+					}
 				}
 			},
 			onStart(battle, source, effect) {
@@ -4798,11 +4805,13 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 			onTerrain(pokemon) {
 				if (pokemon.isGrounded() && !pokemon.isSemiInvulnerable()) {
 					if (pokemon && !pokemon.hasType('Fire')) {
-						this.damage(pokemon.baseMaxhp / 8, pokemon);
-						this.add('-message', `${pokemon.name} was hurt by the terrain!`);
+						if (this.damage(pokemon.baseMaxhp / 8, pokemon)) {
+							this.add('-message', `${pokemon.name} was hurt by the terrain!`);
+						}
 					} else if (pokemon) {
-						this.heal(pokemon.baseMaxhp / 8, pokemon, pokemon);
-						this.add('-message', `${pokemon.name} was healed by the terrain!`);
+						if (this.heal(pokemon.baseMaxhp / 8, pokemon, pokemon)) {
+							this.add('-message', `${pokemon.name} was healed by the terrain!`);
+						}
 					}
 				}
 			},
